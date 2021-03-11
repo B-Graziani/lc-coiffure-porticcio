8ac4bb20ce107875df7960596bae5cde1571821d
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-03-11 17:10:43 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning users..."
+User.destroy_all
+
+puts "Creating users..."
+User1 = { email: "lc-coiffure-porticcio@hotmail.fr", password: "123456" }
+User2 = { email: "benoit@hotmail.fr", password: "123456" }
+
+users = []
+
+
+[ User1, User2 ].each_with_index do |attributes, index|
+  current_user = User.create!(attributes)
+  users << current_user
+end
+puts "Created #{User.count} users"
+
