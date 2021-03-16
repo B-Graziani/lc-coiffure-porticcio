301ec4e2b91e717a4dce98849ae2f412c0800f81
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-03-16 17:47:56 +0100

    enc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,8 @@ require("channels")
 import "bootstrap";
 import { hamburgerResponsive } from'../components/hamburger';
 import { loginModal } from '../components/loginModal';
-// import { newPhoto } from '../components/newPhoto';
+
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -35,5 +36,5 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   hamburgerResponsive();
   loginModal();
-  // newPhoto();
+
 });
