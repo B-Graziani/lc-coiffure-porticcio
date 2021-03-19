6bd299a9e2ac4f917614721b3b80877a9db08f1b
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-03-19 06:01:56 +0100

    enc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,13 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    @article = Article.new
   end
 
   def show
   end
 
-  def new
-    @article = Article.new
-  end
+
 
   def create
     @article = Article.new(article_params)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def devise_mapping
    @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def majuscule(titre)
+    titre.upcase
+  end
 end
