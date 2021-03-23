b46943900472c06f0ead7587e12ad5506d8cd89e
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-03-23 14:11:28 +0100

    enc

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,7 +4,7 @@ console.log("hello batard");
 const infos = document.getElementById("infos");
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
-  if (scroll_position > 100) {
+  if (scroll_position > 20) {
     infos.style.backgroundColor = '#ebd8b7';
   } else {
     infos.style.backgroundColor = 'transparent';
